dab3b69fa0611b35742fa2f7836ecea8657ddc65
Bastien Gorissen <kadomony@gmail.com>
2023-10-04 14:02:05 +0200

    Source code day 3 (part 3)

--- django_project/mysite/blog/views.py
+++ django_project/mysite/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, "blog/post_list.html")
+    published_posts = Post.objects.filter(published_date__lte=timezone.now())
+
+    return render(request, "blog/post_list.html", {"posts": published_posts})
