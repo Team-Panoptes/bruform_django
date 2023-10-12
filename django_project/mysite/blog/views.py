1ad5b3ccd90665a9cea1a80dab29ef66240dae2e
Bastien Gorissen <kadomony@gmail.com>
2023-10-12 12:08:05 +0200

    Day 4 part 3 - Added form to add new posts

--- django_project/mysite/blog/views.py
+++ django_project/mysite/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpRequest
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,6 +12,20 @@ def post_list(request):
 
 
 def post_detail(request, post_number):
-    post = Post.objects.get(id=post_number)
+    post = get_object_or_404(Post, id=post_number)
 
     return render(request, "blog/post_detail.html", {"post": post})
+
+def post_new(request: HttpRequest):
+    
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            blog_post = form.save(commit=False)
+            blog_post.author = request.user
+            blog_post.publish()
+            return redirect("post_detail", post_number=blog_post.id)
+    else:
+        form = PostForm()
+
+    return render(request, "blog/post_new.html", {"form": form})
