5caec48ce7e252119562799e5c067d4734e04947
Bastien Gorissen <kadomony@gmail.com>
2023-10-12 09:52:02 +0200

    Source code day 4 (part 1)

--- django_project/mysite/blog/views.py
+++ django_project/mysite/blog/views.py
@@ -8,3 +8,9 @@ def post_list(request):
     published_posts = Post.objects.filter(published_date__lte=timezone.now())
 
     return render(request, "blog/post_list.html", {"posts": published_posts, "prénom": "Bastien"})
+
+
+def post_detail(request, post_number):
+    post = Post.objects.get(id=post_number)
+
+    return HttpResponse(f"Post number {post_number}")
