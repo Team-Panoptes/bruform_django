8a58400994aeafdb018c925752c801a8204a267c
Bastien Gorissen <kadomony@gmail.com>
2023-10-25 16:45:50 +0200

    Day 8 part 3 - Adding images (not in template yet)

--- wagtail_project/wagtailsite/blog/models.py
+++ wagtail_project/wagtailsite/blog/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.search import index
 
 # Create your models here.
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        blog_posts = self.get_children().live().order_by("-first_published_at")
+        context["blog_posts"] = blog_posts
+        
+        return context
 
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
@@ -29,4 +39,15 @@ class BlogPostPage(Page):
         FieldPanel("date"),
         FieldPanel("intro"),
         FieldPanel("body"),
+        InlinePanel("gallery_images", label="Gallery images"),
+    ]
+
+class BlogPostPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPostPage, on_delete=models.CASCADE, related_name="gallery_images")
+    image = models.ForeignKey("wagtailimages.Image", on_delete=models.CASCADE, related_name="+")
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel("image"),
+        FieldPanel("caption"),
     ]
